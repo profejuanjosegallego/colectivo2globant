737bd87f15414028b5f588579bb65d0eb30df8c2
Snnophy <edwinsmith128@gmail.com>
2025-07-01 09:04:28 -0500

    Se agregaron nuevas cosas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Scanner Escribir = new Scanner(System.in);
 
         System.out.println("RESIDENT EVIL");
         System.out.println("DEVELOPER JJG");
 
         System.out.println("*********RESIDENT EVIL*************");
         System.out.println("Developer SMT");
+
+        String nombre;
+
+        System.out.print("Escriba su nombre: ");
+        nombre = Escribir.next();
     }
 }
