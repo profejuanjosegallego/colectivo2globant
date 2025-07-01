56a2536fa10d8d60fc882e830b19a4c7c75269be
Maria Fernanda Montezuma  Gonzalez <maria.montezuma@globant.com>
2025-07-01 09:04:51 -0500

    feat se crea variable Scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -11,5 +13,6 @@ public class Main {
         System.out.println("DEVELOPER JJG");
         System.out.println(" DEVELOPER JDM");
 
+        Scanner scanner = new Scanner(System.in);
     }
 }
