1896d232da7bf15b018ea04a53fcf1dc824937c4
Andrea Insignares Gaviria <andrea.insignares@globant.com>
2025-07-01 09:04:42 -0500

    feat se crea variable scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -8,6 +10,8 @@ public class Main {
         System.out.println("RESIDENT EVIL");
 
         System.out.println("DEVELOPER JDM");
+
+        Scanner sc=new Scanner(System.in);
 
     }
 }
